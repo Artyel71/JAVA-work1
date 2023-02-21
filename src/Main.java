f59ae644e939fae3d169fd96704fb7cd5e17c1b2
Artyel <artyel@icloud.com>
2023-02-21 16:30:10 +0000

    calculation of engredients

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int eaters = 5; // how many people will eat
+        int water = 3000; // milliliters of water
+        int potatoes = 5; // potatoes
+        int chicken = 6; // chicken thighs
+        int spices = 10; // spoons of spices
+
+        System.out.println("cooked soup. For one person");
+        System.out.println((water / eaters) + "milliliters of water");
+        System.out.println((potatoes / eaters) + "potatoes");
+        System.out.println((chicken / eaters) + "chicken thighs");
+        System.out.println((spices / eaters) + "spoons of spices");
+        
+
+
     }
 }
